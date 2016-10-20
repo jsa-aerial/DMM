3fb1cd9001054d909a857e3b4d1c50e04567ab47
anhinga <lena@ubuntu.(none)>
2016-10-19 21:02:48 -0400

    this codes up aug-27-experiment logic; however we discovered a bug in (rec-map-sum {:a -1} {:a 1}) or (rec-map-sum {:a 1} {:a -1}) not producing 0 as intended, this needs to be fixed before aug-27-experiment logic actually works

--- oct-19-2016-experiment.clj
+++ oct-19-2016-experiment.clj
@@ -3,6 +3,46 @@
 
 (def init-matrix {accum {:self {:accum {accum {:self {:single 1}}}}}})
 
-(def init-output {accum {:self {:single init-matrix}}})
 
+(def update-1-matrix-hook {identity {:update-1 {:single {identity {:update-1 {:single 1}}}}}})
+
+(def update-2-matrix-hook {identity {:update-2 {:single {identity {:update-2 {:single 1}}}}}})
+
+(def update-3-matrix-hook {identity {:update-3 {:single {identity {:update-3 {:single 1}}}}}})
+
+(def start-update-of-network-matrix {accum {:self {:delta {identity {:update-1 {:single 1}}}}}})
+
+(def start-matrix (rec-map-sum-variadic init-matrix update-1-matrix-hook update-2-matrix-hook 
+                                        update-3-matrix-hook start-update-of-network-matrix))
+
+; note that (render-smart start-matrix)
+; now yields
+; {"accum" {:self {:accum {"accum" {:self {:single 1}}}, :delta {"identity" {:update-1 {:single 1}}}}}, "identity" {:update-1 {:single {"identity" {:update-1 {:single 1}}}}, :update-2 {:single {"identity" {:update-2 {:single 1}}}}, :update-3 {:single {"identity" {:update-3 {:single 1}}}}}}
+
+(def update-1-matrix 
+  (rec-map-sum {accum {:self {:delta {identity {:update-1 {:single -1}}}}}}
+               {accum {:self {:delta {identity {:update-2 {:single 1}}}}}}))
+
+(def update-2-matrix 
+  (rec-map-sum {accum {:self {:delta {identity {:update-2 {:single -1}}}}}}
+               {accum {:self {:delta {identity {:update-3 {:single 1}}}}}}))
+
+(def update-3-matrix 
+  (rec-map-sum {accum {:self {:delta {identity {:update-3 {:single -1}}}}}}
+               {accum {:self {:delta {identity {:update-1 {:single 1}}}}}}))
+
+
+; (def init-output {accum {:self {:single init-matrix}}})
+
+(def init-output
+  (rec-map-sum-variadic {accum {:self {:single start-matrix}}}
+                        {identity {:update-1 {:single update-1-matrix}}}
+                        {identity {:update-2 {:single update-2-matrix}}}
+                        {identity {:update-3 {:single update-3-matrix}}}))
+
+(defn extract-matrix [current-output]
+  (((current-output accum) :self) :single))
+
+(defn extract-delta [current-output]
+  ((((extract-matrix current-output) accum) :self) :delta))
 
