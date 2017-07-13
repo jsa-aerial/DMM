2bd5af5648bc000a73f56dc194ec940e25391bad
anhinga <anhinga@users.noreply.github.com>
2017-07-13 11:52:28 -0400

    starting an experiment with recording mouse cliks

---
+++ examples/dmm/quil-controlled/jul_13_2017_experiment.clj
@@ -0,0 +1,122 @@
+(ns dmm.examples.quil-controlled.jul-6-2017-experiment
+  (:require [quil.core :as q]
+            [quil.middleware :as m] 
+            [dmm.core :as dc
+                      :refer [v-accum v-identity
+                              down-movement up-movement
+                              rec-map-sum]]
+            [clojure.core.async :as async]))
+
+(def mouse-pressed-channel (async/chan 10))
+
+(defn mouse-pressed-monitor [dummy]
+  {:signal ((async/alts!! [mouse-pressed-channel] :default {}) 0)})
+
+(def v-mouse-pressed-monitor (var mouse-pressed-monitor))
+
+
+;;; the neurons of this type track mouse position
+;;; we treat X and Y mouse coordinates as separate outputs
+(defn mouse-coords [dummy]
+    {:mouse-x (q/mouse-x) :mouse-y (q/mouse-y)}
+  )
+
+(def v-mouse-coords (var mouse-coords))
+
+(defn init-state []
+  (->
+   {}
+
+   ((fn[m]
+      (assoc m 
+
+             :init-matrix
+             {v-accum {:self {:accum {v-accum {:self {:single 1}}}}}}
+
+             :mouse-tracking-neuron-hook
+             {v-mouse-coords
+              {:mouse-position {:single {v-mouse-coords {:mouse-y 1}}}}}
+
+             :mouse-pressed-monitor-hook
+             {v-mouse-pressed-monitor {:mouse-pressed-monitor {:single 
+              {v-mouse-pressed-monitor {:mouse-pressed-monitor {:single 1}}}}}})))
+
+   ((fn[m]
+      (assoc m :start-matrix
+             (rec-map-sum
+              (m :init-matrix)
+              (m :mouse-tracking-neuron-hook)
+              (m :mouse-pressed-monitor-hook)))))
+   
+   ((fn[m]
+      (assoc m :init-output
+             {v-accum {:self {:single (m :start-matrix)}}})))))
+
+(def state (atom (init-state)))
+
+
+
+(defn setup []
+  ; Set frame rate to 4 frames per second.
+  (q/frame-rate 4)
+  ; setup function returns initial state. It contains
+  ; the initial output layer of the generalized neural network.
+  {:output-layer (@state :init-output)
+   })
+
+(defn update-state [state]
+  ; Update sketch state by performing one cycle of the "two-stroke engine"
+  ; of the generalized neural network.
+  (let [current-input (down-movement (:output-layer state))]
+    {:input-layer current-input
+     :output-layer (up-movement current-input)
+     }))
+
+
+(defn extract-matrix [current-output]
+  (((current-output v-accum) :self) :single))
+
+(defn extract-delta [current-output]
+  ((((extract-matrix current-output) v-accum) :self) :delta))
+
+(defn extract-mouse-position [current-output]
+  (get (current-output v-mouse-coords) :mouse-position "quil quirk"))
+  ; "quil quirk" - the first iteration "draw-state" happens
+  ;                without "update-state" 
+
+(defn extract-mouse-pressed-monitor [current-output]
+  (current-output v-mouse-pressed-monitor))
+
+
+(defn draw-state [state]
+  ; Clear the sketch by filling it with grey color.
+  ;(q/background 127)
+
+  (q/fill 127 50)
+  (q/rect 0 0 (q/width) (q/height))
+  (q/fill 0)
+
+  (q/text-size 24)
+
+  (let [sub-delta-row (->> state :output-layer extract-mouse-pressed-monitor)]
+    (q/text (str (q/frame-count) " " sub-delta-row)  50  (mod (+ 50 (* 25 (q/frame-count))) (q/height)))       
+              ))
+
+(defn mouse-pressed [state event]
+  (async/>!! mouse-pressed-channel event)
+  state)
+
+(q/defsketch quil-try
+  :title "A Quil-controlled DMM"
+  :size [800 600]
+  ; setup function called only once, during sketch initialization.
+  :setup setup
+  ; update-state is called on each iteration before draw-state.
+  :update update-state
+  :draw draw-state
+  :mouse-pressed mouse-pressed
+  :features [:keep-on-top]
+  ; This sketch uses functional-mode middleware.
+  ; Check quil wiki for more info about middlewares and particularly
+  ; fun-mode.
+  :middleware [m/fun-mode])
