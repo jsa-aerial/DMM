989c97c4f3a5f647db609971c38256ff1b824b95
anhinga <michael.bukatin@gmail.com>
2018-01-24 14:29:00 -0500

    upgrading to Clojure 1.9

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
   :url "https://github.com/jsa-aerial/DMM"
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [;;[org.clojure/clojure "1.9.0-alpha16"]
-                 [org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 ;;[org.clojure/clojure "1.8.0"]
                  [quil "2.6.0"]
                  [net.mikera/vectorz-clj "0.45.0"]
                  [org.clojure/tools.nrepl  "0.2.12"] ; Explicit nREPL
                  [proto-repl "0.3.1"] ; for Atom editor
 
-                 [org.clojure/core.async    "0.2.391"]
+                 [org.clojure/core.async    "0.4.474"]
                  ;;[org.clojure/core.async    "0.1.346.0-17112a-alpha"]
                  [org.clojure/data.json     "0.2.6"]
 
@@ -23,7 +23,7 @@
                  [net.apribase/clj-dns      "0.1.0"]] ; reverse-dns-lookup
 
 
-  :plugins [[cider/cider-nrepl "0.12.0"]
+  :plugins [[cider/cider-nrepl "0.16.0"]
             [refactor-nrepl    "2.2.0"]
             [lein-expectations "0.0.8"]
             [lein-gorilla "0.3.6"]]
