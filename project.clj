77c894b15d018d2f475d92e60e0e9636f4cd76d9
jsa-aerial <jsa.aerial@gmail.com>
2018-07-12 17:03:19 -0400

    Add dependencies to use hanasu

--- project.clj
+++ project.clj
@@ -10,18 +10,22 @@
                  [org.clojure/tools.nrepl  "0.2.12"] ; Explicit nREPL
                  [proto-repl "0.3.1"] ; for Atom editor
 
-                 [org.clojure/core.async    "0.4.474"]
-                 ;;[org.clojure/core.async    "0.1.346.0-17112a-alpha"]
-                 [org.clojure/data.json     "0.2.6"]
-
-                 [com.rpl/specter          "0.13.2"]
+                 [http.async.client "1.2.0"]
+                 [http-kit "2.2.0"]
+                 [compojure "1.5.1"]
+                 [org.clojure/core.async "0.4.474"]
                  
-                 [aerial.fs                 "1.1.5"]
-                 [aerial.utils              "1.0.8"]
+                 [clojure-msgpack "1.2.1"]
+                 [org.clojure/data.json "0.2.6"]
+
+                 [com.rpl/specter "1.1.1"]
+                 
+                 [aerial.fs "1.1.5"]
+                 [aerial.utils "1.0.8"]
 
                  [expectations "2.1.8"]
                  [seesaw "1.4.5"]
-                 [net.apribase/clj-dns      "0.1.0"]] ; reverse-dns-lookup
+                 [net.apribase/clj-dns "0.1.0"]] ; reverse-dns-lookup
                  
 
   :plugins [[cider/cider-nrepl "0.16.0"]
